dde4752e7df5053683353d2421be657b1588e4f7
Ricardo Cosme <ricardo@cosme.im>
2021-03-26 23:47:25 -0300

    Adjustments to use avrIO and the new version of ds18b20

--- Makefile
+++ Makefile
@@ -1,46 +1,24 @@
-MCU_TARGET=attiny85
-STD_CXX=c++20
-OPTIMIZE=-Os
+all: thermo.elf
 
-CXX=avr-g++
-CC=avr-gcc
-OBJCOPY=avr-objcopy
-OBJDUMP=avr-objdump
-INCLUDE=-I../att85/include -I../ds18b20/include -I../avrcxx/include
-CXXFLAGS=-std=$(STD_CXX) -g -mmcu=$(MCU_TARGET) -Wall $(OPTIMIZE) -DF_CPU=1000000 $(INCLUDE)
+%.elf: %.cpp
+	avr-g++ -std=c++20 -Os -mmcu=attiny85 -DF_CPU=1000000 -Wall -o $@ $< \
+	../avrcxx/src/avr/watchdog/isr.cpp \
+	-I../att85/include \
+	-I../ds18b20/include \
+	-I../avrcxx/include \
+	-I../avrIO/include \
+	-I../avrWDT/include \
+	-I../avrINT/include
+	avr-size $@
 
-demos = thermo
+%.s: %.elf
+	avr-objdump -d $< > $@
+	cat $@
 
-all: $(demos:%=%.elf) $(demos:%=%.lst) $(demos:%=%.s)
+.PHONY: clean flash
 
-%.d: %.cpp
-	@set -e; rm -f $@; \
-	$(CXX) -MM $(CXXFLAGS) $< > $@.$$$$; \
-	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
-	rm -f $@.$$$$
+flash: thermo.elf
+	avrdude -p t85 -c usbasp -P usb -U flash:w:$<
 
-include $(demos:%=%.d)
-
-%.s: %.cpp
-	$(CXX) $(CXXFLAGS) -S $^
-
-%.elf: %.o
-	$(CXX) $(CXXFLAGS) -o $@ $^ ../avrcxx/src/avrcxx/watchdog/counter.cpp
-
-%.lst: %.elf
-	$(OBJDUMP) -h -S $< > $@
-
-%.hex: %.elf
-	$(OBJCOPY) -j .text -j .data -O ihex $< $@
-
-.PHONY: flash_%
-flash-%: %.hex
-	avrdude -p t85 -c usbasp -P usb  -U flash:w:$<
-
-size:
-	avr-size $(demos:%=%.elf)
-
-.PHONY: clean
 clean:
-	rm -f *.hex *.lst *.elf *.o *.d *.s
-
+	rm -f *.elf *.s
--- thermo.cpp
+++ thermo.cpp
@@ -1,48 +1,42 @@
 #include <att85/ssd1306/display.hpp>
 #include <att85/ssd1306/font/16x32/chars.hpp>
 #include <att85/ssd1306/font/8x8/chars.hpp>
-#include <avrcxx/sleep/power_down.hpp>
+#include <avr/sleep/power_down.hpp>
 #include <ds18b20.hpp>
 
 using namespace att85::ssd1306;
 using namespace att85::ssd1306::commands;
 using namespace ds18b20;
-using namespace avrcxx;
+using namespace avr;
+using namespace avr::io;
 
-inline void disable_unused_pins()
-{ PORTB = PORTB | (1 << PB1) | (1 << PB4) | (1 << PB5); }
-
-template<typename Rom_>
-using thermo = sensor<PB3, Rom_, InternalPullup>;
+inline auto thermo(auto rom)
+{ return sensor{pb3, rom, internal_pullup, resolution::_9bits}; }
 
 int main() {
+    //avoids floating inputs
+    set(pb1, pb4, pb5);
+    
     display_128x64<> disp{contrast, of<1>{}};
     disp.clear();
     disp.on();
-    
-    thermo<Rom<40, 251, 43, 31, 5, 0, 0, 139>> inside;
-    thermo<Rom<40, 198, 8, 141, 5, 0, 0, 124>> outside;
-    
-    using temperature = decltype(inside)::value_type;
-    
+
+    auto inside = thermo(Rom<40, 251, 43, 31, 5, 0, 0, 139>{});
+    auto outside = thermo(Rom<40, 198, 8, 141, 5, 0, 0, 124>{});
+
     disp.out<font::_8x8>(0, 0, ATT85_SSD1306_STR("inside"));
     disp.out<font::_8x8>(3, 40, ATT85_SSD1306_STR("o"));
     
     disp.out<font::_8x8>(0, 64, ATT85_SSD1306_STR("outside"));
     disp.out<font::_8x8>(3, 108, ATT85_SSD1306_STR("o"));
-
-    disable_unused_pins();
-
-    temperature in_temp;
-    temperature out_temp;
+    
     while(true) {
-        if(in_temp = inside.read(); in_temp)
+        if(auto in_temp = inside.read())
             if(in_temp.has_value())
                 disp.out<font::_16x32>(3, 4, in_temp.value());
-        if(out_temp = outside.read(), out_temp)
+        if(auto out_temp = outside.read())
             if(out_temp.has_value())
                 disp.out<font::_16x32>(3, 74, out_temp.value());
-        if(in_temp && out_temp)
-            sleep::power_down::sleep<10min>();
+        sleep::power_down::sleep<10min>();
     }
 }
